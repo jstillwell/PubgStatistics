766d3fb369b49aa5a8c42af12822280dd20e126c
John Stillwell <stillwell.john@gmail.com>
2017-09-06 14:47:41 -0400

    Added a method to get the current season based on date as PUBG does monthly resets now.

---
+++ PubgStatistics.Services/Season.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PubgStatistics.Services {
+    /// <summary>
+    /// Provides methods to help with seasons
+    /// </summary>
+    public class Season {
+        /// <summary>
+        /// Gets the current season string to send the API.
+        /// Handles the monthly reset that started in September or August, I dont remember.
+        /// I assume it started on Sept 1 2017
+        /// </summary>
+        /// <returns></returns>
+        public static string CurrentSeason() {
+            var season4StartDate = new DateTime(2017, 9, 1);
+            var today = DateTime.Now;
+            var monthsSinceSept2017 = ((today.Year - season4StartDate.Year) * 12) + Math.Abs(today.Month - season4StartDate.Month);
+
+            var season = 4 + monthsSinceSept2017;
+
+            return $"{today.Year}-pre{season}";
+        }
+    }
+}
--- PubgStatistics/Controllers/StatsController.cs
+++ PubgStatistics/Controllers/StatsController.cs
@@ -10,49 +10,50 @@ using PUBGSharp;
 using PUBGSharp.Helpers;
 
 namespace PubgStatistics.Controllers {
-  /// <summary>
-  /// Endpoint for PUBG Statistics
-  /// </summary>
-  [Produces("application/json")]
-  [EnableCors("AllowAll")]
-  public class StatsController : Controller {
-    private static readonly string _apiKey = Environment.GetEnvironmentVariable("PubgApiKey");
-
     /// <summary>
-    /// Gets a small subset of the stats from pubgsats.com
+    /// Endpoint for PUBG Statistics
     /// </summary>
-    /// <param name="userId">The user ID that you want</param>
-    /// <returns></returns>
-    [HttpGet]
-    [Route("api/stats/{userId}")]
-    [ProducesResponseType(200)]
-    public async Task<ActionResult> GetStats(string userId) {
-      using (var client = new PUBGStatsClient(_apiKey)) {
-        var stats = await client.GetPlayerStatsAsync(userId).ConfigureAwait(false);
-        var na = stats.Stats.Find(s => s.Region == PUBGSharp.Data.Region.NA);
+    [Produces("application/json")]
+    [EnableCors("AllowAll")]
+    public class StatsController : Controller {
+        private static readonly string _apiKey = Environment.GetEnvironmentVariable("PubgApiKey");
 
-        var solo = stats.Stats.Find(s => s.Region == PUBGSharp.Data.Region.NA && s.Mode == PUBGSharp.Data.Mode.Solo);
-        var duos = stats.Stats.Find(s => s.Region == PUBGSharp.Data.Region.NA && s.Mode == PUBGSharp.Data.Mode.Duo);
-        var squads = stats.Stats.Find(s => s.Region == PUBGSharp.Data.Region.NA && s.Mode == PUBGSharp.Data.Mode.Squad);
+        /// <summary>
+        /// Gets a small subset of the stats from pubgsats.com
+        /// </summary>
+        /// <param name="userId">The user ID that you want</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/stats/{userId}")]
+        [ProducesResponseType(200)]
+        public async Task<ActionResult> GetStats(string userId) {
+            using (var client = new PUBGStatsClient(_apiKey)) {
+                var stats = await client.GetPlayerStatsAsync(userId).ConfigureAwait(false);
+                var na = stats.Stats.Find(s => s.Region == PUBGSharp.Data.Region.NA);
+                var currentSeason = Services.Season.CurrentSeason();
 
-        var minimalStats = new Models.MinimalStats() {
-          Solo = new List<string> {
+                var solo = stats.Stats.Find(s => s.Region == PUBGSharp.Data.Region.NA && s.Mode == PUBGSharp.Data.Mode.Solo && s.Season == currentSeason);
+                var duos = stats.Stats.Find(s => s.Region == PUBGSharp.Data.Region.NA && s.Mode == PUBGSharp.Data.Mode.Duo && s.Season == currentSeason);
+                var squads = stats.Stats.Find(s => s.Region == PUBGSharp.Data.Region.NA && s.Mode == PUBGSharp.Data.Mode.Squad && s.Season == currentSeason);
+
+                var minimalStats = new Models.MinimalStats() {
+                    Solo = new List<string> {
                     solo.Stats.Find(x => x.Stat == Stats.KDR).Value,
                     solo.Stats.Find(x => x.Stat == Stats.Wins).Value
                     },
-          Duos = new List<string> {
+                    Duos = new List<string> {
                         duos.Stats.Find(x => x.Stat == Stats.KDR).Value,
                         duos.Stats.Find(x => x.Stat == Stats.Wins).Value
                     },
-          Squads = new List<string> {
+                    Squads = new List<string> {
                         squads.Stats.Find(x => x.Stat == Stats.KDR).Value,
                         squads.Stats.Find(x => x.Stat == Stats.Wins).Value
                     }
-        };
+                };
 
-        var json = JsonConvert.SerializeObject(minimalStats);
-        return Ok(json);
-      };
+                var json = JsonConvert.SerializeObject(minimalStats);
+                return Ok(json);
+            };
+        }
     }
-  }
 }
